6ae92a76335b8b67f31e747b986365065983558f
Eleonora <eleonora.digregorio8@gmail.com>
2019-09-27 17:18:42 +0200

    testing

--- Lecture01/calculator.py
+++ Lecture01/calculator.py
@@ -1,35 +1,38 @@
-def sum(m,n):
-    if n < 0 :
+def sum(m, n):
+    if n < 0:
         for x in range(abs(n)):
-            m -=1
-    else: 
+            m -= 1
+    else:
         for x in range(0, n):
-            m +=1
+            m += 1
     return m
 
-def subtract(m,n):
+
+def subtract(m, n):
     return sum(m, -n)
-    
-    
-def divide(m,n):
+
+
+def divide(m, n):
     count = 0
-    negativeResult = m > 0 and n < 0 or m < 0 and  n > 0 
+    negativeResult = m > 0 and n < 0 or m < 0 and n > 0
     nabs = abs(n)
     mabs = abs(m)
     if n != 0:
-        while ( mabs >= nabs ):
+        while (mabs >= nabs):
             mabs -= nabs
-            count+=1
+            count += 1
         if negativeResult:
-            count = -count   
+            count = -count
+    else:
+        raise ZeroDivisionError()
     return count
 
-def multiply(m,n):
-    negativeResult = m > 0 and n < 0 or m < 0 and  n > 0  
+
+def multiply(m, n):
+    negativeResult = m > 0 and n < 0 or m < 0 and n > 0
     result = 0
     for x in range(abs(n)):
-        result += abs (m)
+        result += abs(m)
     if negativeResult:
         result = -result
-    return result 
-
+    return result
